c570a4e00e9b7d5b22e5e86395a0d639bf15822d
정연진 <dus532@naver.com>
2022-08-23 20:39:01 +0900

    Feat: 폰트 설정, 메인 페이지 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import { x } from '@xstyled/emotion';
 
+import Logo from '@/assets/svgs/Logo';
+
 function App() {
   return (
     <>
-      <x.div w="100%" h="56px" backgroundColor="white">
-        1
+      <x.div w="100%" marginTop="28px" justifyContent="center" display="flex">
+        <Logo />
+      </x.div>
+      <x.div
+        fontSize="1.5rem"
+        fontWeight={600}
+        textAlign="center"
+        marginTop="12px"
+      >
+        안녕하세요!
+        <br />
+        이용하시겠어요?
+      </x.div>
+      <x.div
+        marginTop="8px"
+        fontSize="0.9rem"
+        textAlign="center"
+        color="#D4D4D8"
+      >
+        먼저 로그인해주세요.
       </x.div>
     </>
   );
