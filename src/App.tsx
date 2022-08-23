18e9f90c830a67754c1b8ded64c4349d62482454
정연진 <dus532@naver.com>
2022-08-23 21:55:37 +0900

    Feat: 정리

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Input from '@/components/molecules/Input';
 
 function App() {
   const [inputStep, setInputStep] = useState(0);
-  const [text, setText] = useState('');
   const { register, handleSubmit, setFocus } = useForm<{
     name: string;
     phoneNumber: string;
@@ -63,7 +62,6 @@ function App() {
               placeholder="이름을 입력하세요"
               marginBottom="24px"
               onKeyUp={(e) => {
-                setText(e.key);
                 if (e.key == 'Enter') {
                   setFocus('phoneNumber');
                   setInputStep(1);
@@ -71,7 +69,6 @@ function App() {
               }}
             />
           </x.div>
-          {text}
           <x.div
             opacity={0}
             animation={inputStep > 0 ? 'slideIn 1s forwards' : ''}
