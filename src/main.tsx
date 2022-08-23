f9ae1d2b290e5cf2ed8d8ec21ed993711be1e0dc
정연진 <dus532@naver.com>
2022-08-23 20:12:36 +0900

    Init: eslint 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
+import 'minireset.css';
+
+import { defaultTheme, ThemeProvider, Preflight } from '@xstyled/emotion';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const theme = {
+  ...defaultTheme,
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <Preflight />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
 );
